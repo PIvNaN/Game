209fc442ac04a5870771589f5e08970f414bdf1c
Iva Petkova <iva.g.petkova@abv.bg>
2018-03-06 22:56:52 +0200

    Initialize game canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
+/**
+ * Initialize game canvas
+ */
+
 import player from './elements/player.js';
+
+let canvas = document.createElement('canvas');
+canvas.classList.add('player-canvas');
+
+document.querySelector('.app-container').appendChild(canvas);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+/**
+ * Call game elements
+ */
 
 player();
