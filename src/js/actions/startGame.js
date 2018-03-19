6c1f94e62a77278b0ec01527b510af9586a2da96
Iva Petkova <iva.g.petkova@abv.bg>
2018-03-20 01:13:58 +0200

    Replace update, clear and render methods about player in to player class

--- src/js/actions/startGame.js
+++ src/js/actions/startGame.js
@@ -1,83 +1,36 @@
 'use strict';
 
-let frameIndex = 0;
-let framesCount = 4;
-
 let loopTicksCount = 0;
-let loopTicksPerFrame = 7;
 
-let x = 0;
-let y = 0;
-
-let lastX;
-let lastY;
-
-let speedX = 4;
+import Player from "../elements/player.js";
 
 export default function startGame(image, context, canvas) {
-
+	let player = new Player(context, canvas);
+	// console.log(image.width)
 	loop();
 
 	function loop() {
 		requestAnimationFrame(loop);
 
-		update();
+		_update();
 
-		clear();
+		_clear();
 
-		render();
+		_render();
 
 		loopTicksCount += 1;
 	}
 
-	function update() {
-		if (loopTicksCount >= loopTicksPerFrame) {
-
-			loopTicksCount = 0;
-			frameIndex += 1;
-
-			if (frameIndex >= framesCount) {
-				frameIndex = 0;
-			}
-		}
-
-		lastX = x;
-		lastY = y;
-
-		x += speedX;
-
-		if(x >= (canvas.width - (image.width / framesCount))) {
-			speedX = -speedX;
-		}
-
-		if(x <= 0) {
-			speedX = -speedX;
-		}
-		
+	function _update() {
+		player.update(image)
 	}
 
-	function clear() {
-		context.clearRect(
-			lastX,
-			lastY,
-			image.width / framesCount,
-			image.height / 2
-		);
+	function _clear() {
+		player.clear(image)
 	}
 
-	function render() {
-
-		context.drawImage(
-			image,
-			frameIndex * image.width / framesCount,
-			0,
-			image.width / framesCount,
-			image.height / 2,
-			x,
-			y,
-			image.width / framesCount,
-			image.height / 2
-		);
+	function _render() {
+		player.render(image)
 	}
 
 }
--- src/js/elements/player.js
+++ src/js/elements/player.js
@@ -6,13 +6,104 @@ import startGame from '../actions/startGame.js'
 class Player {
 
 	constructor(context, canvas) {
+
 		this.loadImage(context, canvas);
+
+		let frameIndex = 0;
+		let framesCount = 4;
+
+		let loopTicksCount = 0;
+		let loopTicksPerFrame = 7;
+		
+		let x = 0;
+		let y = 0;
+
+		let lastX;
+		let lastY;
+		
+		let speedX = 4;
+
+		this.frameIndex = frameIndex;
+		this.framesCount = framesCount;
+
+		this.loopTicksCount = loopTicksCount;
+		this.loopTicksPerFrame = loopTicksPerFrame;
+
+		this.x = x;
+		this.y = y;
+
+		this.lastX;
+		this.lastY;
+
+		this.speedX = speedX;
+
+		this.context = context;
+		this.canvas = canvas;		
 	}
 	
 	loadImage(context, canvas) {
 		loadImage('../images/running-cat.png', (image) => {
 			startGame(image, context, canvas);
 		})
+	}
+	update(image) {
+		console.log('Is Update START?', this.canvas, this.loopTicksPerFrame, this.lastX);		
+
+		if (this.loopTicksCount >= this.loopTicksPerFrame) {
+
+			this.loopTicksCount = 0;
+			this.frameIndex += 1;
+
+			if (this.frameIndex >= this.framesCount) {
+				this.frameIndex = 0;
+			}
+		}
+
+		this.lastX = this.x;
+		this.lastY = this.y;
+
+		this.x += this.speedX;
+
+		if(this.x >= (this.canvas.width - (image.width / this.framesCount))) {
+			this.speedX = -this.speedX;
+		}
+
+		if(this.x <= 0) {
+			this.speedX = -this.speedX;
+		}
+
+		console.log('Is Update DONE?');
+	}
+
+	clear(image) {
+		console.log('Is Clear START?');		
+		
+		this.context.clearRect(
+			this.lastX,
+			this.lastY,
+			image.width / this.framesCount,
+			image.height / 2
+		);
+
+		console.log('Is Clear DONE?');
+	}
+
+	render(image) {
+		console.log('Is Render START?');		
+
+		this.context.drawImage(
+			image,
+			this.frameIndex * image.width / this.framesCount,
+			0,
+			image.width / this.framesCount,
+			image.height / 2,
+			this.x,
+			this.y,
+			image.width / this.framesCount,
+			image.height / 2
+		);
+
+		console.log('Is Render DONE?');
 	}
 
 }
